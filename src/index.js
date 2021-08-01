2a6899a918da1b8cd13498a6aa0500257031c560
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-07-31 23:42:42 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,9 +18,9 @@ function Square(props){
     return (
       <button 
         className="square" 
-        onClick={() => this.props.onClick()}
+        onClick={() => props.onClick()}
       >
-        {this.props.value}
+        {props.value}
       </button>
     );
   }
@@ -29,14 +29,18 @@ class Board extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     }
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice()
-    squares[i] = 'X'
-    this.setState({squares: squares})
+    squares[i] = this.state.xIsNext ? 'X' : 'O'
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    })
   } 
 
   renderSquare(i) {
